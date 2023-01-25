9b02656bbb3a1b3a4b0221db485dbcc9c4fffe7c
Nicholas Chug <nicholas.chug@gmail.com>
2023-01-25 12:43:04 -0500

    finalizing work and finished submission actions for form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,11 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent='handleSubmit'>
 
     <label>Email:</label>
     <input type='email' required v-model='email'>
 
     <label>Password:</label>
     <input type='password' required v-model='password'>
+    <div v-if='passwordError' class='error'>{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model='role'>
@@ -15,17 +16,22 @@
 
     <label>Skills:</label>
     <input type="text" v-model='tempSkill' @keyup.alt='addSkill'>
-    <div v-for='skill in skills' :key='skill' class='pill'>{{ skill }}</div>
+    <div v-for='skill in skills' :key='skill' class='pill'>
+        <span @click='deleteSkill(skill)'>
+            {{ skill }}
+        </span>
+    </div>
 
     <div class="terms">
         <input type="checkbox" required v-model='terms'>
         <label>Accept Terms & Conditions</label>
     </div>
 
+    <div class="submit">
+        <button>Create an Account</button>
+    </div>
+
   </form>
-  <p>Email: {{ email }}</p>
-  <p>Password: {{ password }}</p>
-  <p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -37,7 +43,8 @@ export default {
             role: '',
             terms: false,
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: '',
         }
     },
     methods: {
@@ -47,6 +54,22 @@ export default {
                     this.skills.push(this.tempSkill);
                 }
                 this.tempSkill = '';
+            }
+        },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return skill !== item
+            });
+        },
+        handleSubmit() {
+            // validate password
+            this.passwordError = this.password.length > 5 ? '' : 'Password must be at least 6 chars long'
+
+            if (!this.passwordError) {
+                console.log(this.email)
+                console.log(this.password)
+                console.log(this.role)
+                console.log(this.skills)
             }
         }
     }
@@ -87,4 +110,33 @@ export default {
     position: relative;
     top: 2px;
   }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+}
+button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+.submit {
+    text-align: center;
+}
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+}
 </style>
